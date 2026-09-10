2330e77cc1f85e9390809d367568ef08a7ff07f0
Guilherme Nascimento <borutoplays038@gmail.com>
2026-09-10 20:14:45 -0300

    feat: Adicionando o footer estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TaskListSimples from './components/TaskListSimples_1'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
     <Header/>
     <TaskListSimples/>
+    <Footer/>
     </>
   )
 }
